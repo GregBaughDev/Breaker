0fa5e8042ccf3d2b7645fce0c55cc6a399d08739
Greg <80447308+GregBDesign@users.noreply.github.com>
2023-06-08 16:27:31 +1000

    Initial commit

---
+++ core/src/com/breaker/game/BreakerGame.java
@@ -0,0 +1,23 @@
+package com.breaker.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class BreakerGame extends ApplicationAdapter {
+	@Override
+	public void create () {
+
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 0, 0, 1);
+	}
+	
+	@Override
+	public void dispose () {
+
+	}
+}
---
+++ desktop/src/com/breaker/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.breaker.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.breaker.game.BreakerGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Breaker");
+		new Lwjgl3Application(new BreakerGame(), config);
+	}
+}
