52971b6ff48a81dcb39ee6ef9dec0c137087b323
Greg <80447308+GregBDesign@users.noreply.github.com>
2023-06-09 20:50:25 +1000

    Update gameloop

--- core/src/com/breaker/game/BreakerGame.java
+++ core/src/com/breaker/game/BreakerGame.java
@@ -1,23 +1,28 @@
 package com.breaker.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.breaker.game.components.Paddle;
 
 public class BreakerGame extends ApplicationAdapter {
+	Paddle paddle;
+
 	@Override
 	public void create () {
-
+		// Runs once when game starts
+		paddle = new Paddle();
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		// game loop - runs every frame
+		ScreenUtils.clear(0, 0, 0, 1);
+		paddle.draw();
+		paddle.handleInput();
 	}
 	
 	@Override
 	public void dispose () {
-
+		// clean up
 	}
 }
