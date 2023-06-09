20c107a9c3883538c795290b28633034fbc26b9a
Greg <80447308+GregBDesign@users.noreply.github.com>
2023-06-09 20:50:41 +1000

    Update launcher

--- desktop/src/com/breaker/game/DesktopLauncher.java
+++ desktop/src/com/breaker/game/DesktopLauncher.java
@@ -6,10 +6,15 @@ import com.breaker.game.BreakerGame;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
+	static final int WINDOW_WIDTH = 800;
+	static final int WINDOW_HEIGHT = 600;
+
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Breaker");
+		config.setWindowedMode(WINDOW_WIDTH, WINDOW_HEIGHT);
+		config.setResizable(false);
 		new Lwjgl3Application(new BreakerGame(), config);
 	}
 }
