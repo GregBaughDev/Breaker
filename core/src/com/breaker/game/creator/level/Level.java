041034f03ca5476efcc66502bb38260c51b846e7
Greg <80447308+GregBDesign@users.noreply.github.com>
2023-06-17 20:46:18 +1000

    Level

--- core/src/com/breaker/game/creator/level/Level.java
+++ core/src/com/breaker/game/creator/level/Level.java
@@ -1,6 +1,8 @@
 package com.breaker.game.creator.level;
 
-public interface Level {
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+public class Level {
     // Maybe we could have rows for each 100px of the game area
     // as a grid?
     // 800px width = 10 x 8
@@ -8,6 +10,37 @@ public interface Level {
     // 100px at the bottom for paddle
     // 100px between paddle and bricks
     // 400px for bricks
-    // this is a nested array
     // also make it extensible so the vals are not hardcoded
+
+    // store levels as json
+
+    Boolean[][] structure = {
+            {true, true, false, true, false, false, true, true},
+            {true, true, true, false, true, true, false, false},
+            {false, true, true, true, true, true, false, false},
+            {false, true, true, true, true, true, true, false},
+            {true, true, false, true, false, false, true, true},
+            {true, true, true, false, true, true, false, false},
+            {false, true, true, true, true, true, false, false},
+        };
+
+    int levelNumber = 1;
+
+    public void Level() {}
+
+    public Boolean getValue(int outer, int inner) {
+        return structure[outer][inner];
+    }
+
+    public void setStructure(int outer, int inner, Boolean value) {
+        this.structure[outer][inner] = value;
+    }
+
+    public Boolean[][] getStructure() {
+        return this.structure;
+    }
+
+    public int getLevelNumber() {
+        return this.levelNumber;
+    }
 }
