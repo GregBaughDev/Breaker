cb9ce253567561dec6360097fc437339f1c1b886
Greg <80447308+GregBDesign@users.noreply.github.com>
2023-06-25 17:53:52 +1000

    Refactoring

--- core/src/com/breaker/game/creator/level/LevelManager.java
+++ core/src/com/breaker/game/creator/level/LevelManager.java
@@ -1,27 +1,61 @@
 package com.breaker.game.creator.level;
 
 import com.badlogic.gdx.utils.Json;
+import com.breaker.game.components.Brick;
 import com.breaker.game.utils.FileReader;
+
+import java.util.List;
 
 public class LevelManager {
     FileReader fileReader;
     Json json;
     Level level;
+    Integer currentLevel;
 
     public LevelManager() {
         this.fileReader = new FileReader();
         this.json = new Json();
+        this.currentLevel = 0;
+        this.setLevel();
     }
 
-    public Level getCurrentLevel() {
+    public Level getLevel() {
         return this.level;
     }
 
-    public void setCurrentLevel(String level) {
-        this.level = json.fromJson(Level.class, getLevelFileContent(level));
+    public void incrementAndSetLevel() {
+        this.incrementCurrentLevel();
+        this.setLevel();
+    }
+
+    public void setupLevel(List<Brick> bricks) {
+        int x = 0;
+        int y = 200;
+        for (int i = 0; i < getLevel().getStructure().length; i++) {
+            for (int j = 0; j < getLevel().getStructure()[i].length; j++) {
+                if (LevelHelper.convertToBoolean(getLevel().getValue(i, j))) {
+                    bricks.add(new Brick(x, y));
+                }
+                x += 90;
+            }
+            x = 0;
+            y += 30;
+        }
+    }
+
+    private void setLevel() {
+        this.level = json.fromJson(Level.class, getLevelFileContent(this.currentLevel.toString()));
+    }
+
+    private void incrementCurrentLevel() {
+        this.currentLevel++;
     }
 
     private String getLevelFileContent(String level) {
         return fileReader.getLevelFileContent(level);
+    }
+
+    private void levelFileCount() {
+        // TO DO - Get number of files from fileReader
     }
 }
--- core/src/com/breaker/game/state/BreakerState.java
+++ core/src/com/breaker/game/state/BreakerState.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.breaker.game.components.Ball;
 import com.breaker.game.components.Brick;
 import com.breaker.game.components.Paddle;
-import com.breaker.game.creator.level.LevelHelper;
 import com.breaker.game.creator.level.LevelManager;
 
 import java.io.FileNotFoundException;
@@ -19,14 +18,12 @@ public class BreakerState {
     LevelManager levelManager;
     // leave below for min
 //    InfoDisplay infoDisplay;
-    Integer currentLevel;
     Integer brickGapX;
 
     public BreakerState() {
         this.paddle = new Paddle();
         this.ball = new Ball();
         this.bricks = new ArrayList<>();
-        this.currentLevel = 0;
         this.levelManager = new LevelManager();
         this.brickGapX = 10;
     }
@@ -43,23 +40,6 @@ public class BreakerState {
         return this.bricks;
     }
 
-    public void setupLevel() throws FileNotFoundException {
-        // TO DO - Use level manager to write a function to find number of levels in folder
-        levelManager.setCurrentLevel(currentLevel.toString());
-        int x = 0;
-        int y = 200;
-        for (int i = 0; i < levelManager.getCurrentLevel().getStructure().length; i++) {
-            for (int j = 0; j < levelManager.getCurrentLevel().getStructure()[i].length; j++) {
-                if (LevelHelper.convertToBoolean(levelManager.getCurrentLevel().getValue(i, j))) {
-                    bricks.add(new Brick(x, y));
-                }
-                x += 90;
-            }
-            x = 0;
-            y += 30;
-        }
-    }
-
     public void handleRender() throws FileNotFoundException {
         ScreenUtils.clear(0, 0, 0, 1);
         getPaddle().draw();
@@ -68,14 +48,20 @@ public class BreakerState {
         getPaddle().handleInput();
         getBall().update(getPaddle().isCollision(getBall()));
         handleBricks();
+
         if (this.getBall().isOut(getPaddle().getY())) {
             getBall().resetBall();
         }
+
         if (this.getBricks().isEmpty()) {
-            incrementLevel();
-            setupLevel();
+            levelManager.incrementAndSetLevel();
+            this.setupLevel();
             getBall().resetBall();
         }
+    }
+
+    public void setupLevel() throws FileNotFoundException {
+        this.levelManager.setupLevel(bricks);
     }
 
     private void handleBricks() {
@@ -87,9 +73,5 @@ public class BreakerState {
             }
         }
         this.getBricks().removeIf(Brick::getIsDestroyed);
-    }
-
-    private void incrementLevel() {
-        this.currentLevel++;
     }
 }
